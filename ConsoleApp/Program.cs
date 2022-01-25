c1cbb9c0ba6d7d37aa34243be6ae6fd2d06786eb
eddyjav <eddy.j.av@gmail.com>
2022-01-24 19:23:34 -0500

    Commit Final - Carga de Datos
    Carga de datos Rental, Customer, Payment

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -94,11 +94,54 @@ namespace ConsoleApp
                 Payments = new List<Payment>()
             };
 
+            Customer customer2 = new Customer()
+            {
+                store_Id = 2,
+                first_name = "Jose",
+                last_name = "Mejia",
+                email = "jm@gmail.com",
+                addres_Id = 2,
+                activebool = true,
+                create_date = DateTime.Now,
+                last_update = DateTime.Now,
+                active = false,
+                Rental = rental2,
+                Payments = new List<Payment>()
+            };
+            Customer customer3 = new Customer()
+            {
+                store_Id = 3,
+                first_name = "Andrea",
+                last_name = "Samaniego",
+                email = "as@gmail.com",
+                addres_Id = 3,
+                activebool = true,
+                create_date = DateTime.Now,
+                last_update = DateTime.Now,
+                active = false,
+                Rental = rental3,
+                Payments = new List<Payment>()
+            };
+
+            Customer customer4 = new Customer()
+            {
+                store_Id = 4,
+                first_name = "Carolina",
+                last_name = "Espinoza",
+                email = "ce@gmail.com",
+                addres_Id = 4,
+                activebool = true,
+                create_date = DateTime.Now,
+                last_update = DateTime.Now,
+                active = false,
+                Rental = rental4,
+                Payments = new List<Payment>()
+            };
 
             //PAYMENT
             Payment payment1 = new Payment()
             {
-                customerId = 1,
+                
                 staff_id = 1,
                 amount = 25.50f,
                 payment_date = DateTime.Now,
@@ -106,10 +149,64 @@ namespace ConsoleApp
                 Rental = rental1
             };
 
+            
+            Payment payment2 = new Payment()
+            {
+                
+                staff_id = 2,
+                amount = 25.50f,
+                payment_date = DateTime.Now,
+                Customer = customer2,
+                Rental = rental2
+            };
+
+            Payment payment3 = new Payment()
+            {
+                
+                staff_id = 1,
+                amount = 25.50f,
+                payment_date = DateTime.Now,
+                Customer = customer3,
+                Rental = rental3
+            };
+
+            Payment payment4 = new Payment()
+            {
+                
+                staff_id = 4,
+                amount = 25.50f,
+                payment_date = DateTime.Now,
+                Customer = customer4,
+                Rental = rental4
+            };
+
+            Payment payment5 = new Payment()
+            {
+                
+                staff_id = 5,
+                amount = 25.50f,
+                payment_date = DateTime.Now,
+                Customer = customer4,
+                Rental = rental5
+            };
+
+            Payment payment6 = new Payment()
+            {
+                
+                staff_id = 6,
+                amount = 25.50f,
+                payment_date = DateTime.Now,
+                Customer = customer3,
+                Rental = rental6
+            };
+            
 
 
             Repositorio db = new Repositorio();
             db.Payments.Add(payment1);
+            db.Payments.Add(payment2);
+            db.Payments.Add(payment3);
+            db.Payments.Add(payment4);            
             db.SaveChanges();
 
 
